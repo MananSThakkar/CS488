74b5a10317dd0bc95f9122dd9226a5ce519dff7c
Manan Thakkar <32281699+MananSThakkar@users.noreply.github.com>
2019-03-05 19:23:30 -0500

    Add files via upload

--- ReceiverRUDP.java
+++ ReceiverRUDP.java
@@ -9,9 +9,15 @@ import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 
 public class ReceiverRUDP {
-	final static int ownPort = 888;
-	final static int targetPort = 777;
+	final static int ownPort = 2220;
+	final static int targetPort = 1110 ;
 	static InetAddress host = null;
+	
+	static int packetNumber = 0;
+    static int lastPacket = 0;
+    static boolean isLastPacket = false;
+    static boolean isLastMessage = false;
+    
 	static{
 		try {
 			host = InetAddress.getByName("localhost");
@@ -22,12 +28,112 @@ public class ReceiverRUDP {
 	}
 
 	public static void main(String[] args) throws IOException, InterruptedException {
-	        File file = new File("./Resource/copy_1_udp.jpg");
+	        File file = new File("./Resource/copy_1.2_rudp.jpg");
 	        FileOutputStream fis = new FileOutputStream(file);
-	        byte[] data = new byte[1024];
 	        DatagramSocket datagramSocket = new DatagramSocket(ownPort);
+	        System.out.println("Receiver: connection built, about to transfer. ");
+	        
+	        while(!isLastMessage) {
+	        	byte[] bytes = new byte[1024];
+	        	byte[] data = new byte[1021];
+	        	DatagramPacket receivePacket = new DatagramPacket(bytes, bytes.length);
+	        	datagramSocket.setSoTimeout(0);
+	        	datagramSocket.receive(receivePacket);
+	        	
+	        	packetNumber = ((bytes[0]) << 8) + (bytes[1]);
+	        	
+	        	if(bytes[2] == 255 && bytes[3] == 255) {
+	        		isLastPacket = true;
+				}else { isLastPacket = false;}
+				
+				if(packetNumber == (lastPacket + 1)){
+					lastPacket = packetNumber;
+					
+					
+					for(int i = 4; i < 1024; i++) {
+						data[i-4] = bytes[i];
+					}
+					fis.write(data);
+					System.out.println("Received Packet Number " + lastPacket);
+					byte[] acks = new byte[2];
+					acks[0] = (byte) (lastPacket >> 8);
+					acks[1] = (byte) (lastPacket); 
+					DatagramPacket ack = new DatagramPacket(acks, acks.length, host, receivePacket.getPort());
+					datagramSocket.send(ack);
+					System.out.println("Ack Sent");
+	        }else {
+	        	System.out.println("Resending Ack " + packetNumber);
+	        	byte[] acks = new byte[2];
+				acks[0] = (byte) (lastPacket >> 8);
+				acks[1] = (byte) (lastPacket); 
+				DatagramPacket ack = new DatagramPacket(acks, acks.length, host, receivePacket.getPort());
+				datagramSocket.send(ack);
+				System.out.println("Ack Sent");  	
+	        }
+				if(isLastPacket) {
+					isLastMessage = false;
+					break;
+				}
+	        
+	        }
+	        datagramSocket.close();
+	        fis.close();
+	        System.out.println("File has been received");
+	}
+}
+	        
+	        
+	        
+				/*    
+	        
+	        byte[] data = new byte[1024];
+	       
+	        
+	        DatagramPacket dataPacket = new DatagramPacket(data, data.length);
+	        datagramSocket.receive(dataPacket);
+	        String st = new String(dataPacket.getData(),dataPacket.getOffset(), dataPacket.getLength() );
+	        int length = Integer.parseInt(st.trim());
+	        System.out.println("Size of the incoming file:  " + length + " bytes");
+	        
+	        int count = 0 ;
+	        int seqnum = 0;
+	        while(count < ((length/1024)+1) && length > 0) {
+	        	try {
+	        		byte[] buff = new byte[1024];
+	        		DatagramPacket receivePacket = new DatagramPacket(buff, buff.length);
+	        		System.out.println("Receiving pakcet...");
+	        		datagramSocket.receive(receivePacket);	        		
+	        		fis.write(buff);
+	        		int lastseqnum = seqnum - 1;
+	        		if(receivePacket.getLength() <= 0) {
+	        			System.out.println("Packet Lost" + lastseqnum);
+	        		}else {
+	        		
+	        		datagramSocket.setSoTimeout(10000);
+	        		
+	        		System.out.println("ACK " + count + "Sequence number " + seqnum + " received");
+	        		String m = Integer.toString(count);
+	        		byte[] acks = m.getBytes();
+                    DatagramPacket ackdata = new DatagramPacket(acks, acks.length, receivePacket.getAddress(), receivePacket.getPort());
+                    datagramSocket.send(ackdata);
+	        		
+	        		
+	        	}
+	        	count++;
+	        	seqnum++;
+	        	}			
+	        	catch(SocketTimeoutException e){
+	        		System.out.println("Error!");
+	        		
+	        		
+	        	}
+	        	if(count == (length/1024)+1) {
+	        		break;
+	        	}
+	        }
+	        
 	       // ServerSocket socket = new ServerSocket(ownPort, 1, host);
-	        datagramSocket.setSoTimeout(10000);
+	       datagramSocket.setSoTimeout(10000);
 	        System.out.println("Receiver: connection built, about to transfer. ");
 	        while(true) {
 	        	try {
@@ -45,5 +151,5 @@ public class ReceiverRUDP {
 	        System.out.println("Receiver: transfer completed");
 	        datagramSocket.close();
 	        fis.close();
-		}
-}
+	        */
+		
--- SenderRUDP.java
+++ SenderRUDP.java
@@ -4,17 +4,25 @@ import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
+
+
 public class SenderRUDP {
-	final static int ownPort = 777;
-	final static int targetPort = 888;
+	final static int ownPort = 1110;
+	final static int targetPort = 2220;
 	static InetAddress host = null;
+	
+	static int fileLength;
+    static int packetNumber = 0;
+    static int ackPacketNo = 0;
+    static boolean LastPacket = false;
+
+    static boolean AckReceived = false;  //Ack received from receiver.
+	
 	static{
 		try {
 			host = InetAddress.getByName("localhost");
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -22,19 +30,72 @@ public class SenderRUDP {
 	public static void main(String[] args) throws IOException, InterruptedException {
 	        File file = new File("./Resource/1.jpg");
 	        FileInputStream fis = new FileInputStream(file);
-			byte[] data = new byte[1024];
-			// Socket socket = new Socket(host.getHostAddress(), targetPort);
-			DatagramSocket datagramSocket = new DatagramSocket(ownPort);
+	        fileLength = (int) file.length();
+	       
+	        byte[] data = new byte[fileLength];
+	        fis.read(data);
+	        
+	        DatagramSocket datagramSocket = new DatagramSocket(ownPort);
 			System.out.println("Sender: connection built, about to transfer");
-			while(fis.read(data) != -1) {
-			// 	socket.getOutputStream().write(data);
-				DatagramPacket sendPacket = new DatagramPacket(data, data.length, host, targetPort);
+			System.out.println("Sending the file");
+	       
+	        for(int byteInPackets=0; byteInPackets<data.length; byteInPackets+=1020) {
+	        	packetNumber++;
+	        	
+	        	byte[] bytes = new byte[1024]; //
+	        	bytes[0] = (byte) (packetNumber >> 8);// 1 byte that is 8 bits for the head
+	        	bytes[1] = (byte) (packetNumber); //Second byte is for SEQ number
+	        	
+	        	
+	        	if((byteInPackets+1021) >= data.length) {
+	        		LastPacket = true; //Last packet 
+	        		bytes[2] = (byte) 255;
+	        		bytes[3] = (byte) 255;
+	        	}else {
+	        		LastPacket = false;
+	        		bytes[2] = (byte) 0;
+	        		bytes[3] = (byte) 0;
+	        	}
+	        	
+	        	if(!LastPacket) {
+	        		for(int index = 0; index <= 1019; index++)
+	        		bytes[index + 4] = data[byteInPackets+index];
+	        	}else if(LastPacket){
+	        		for(int index = 0; index < (data.length-byteInPackets); index++)
+	        		bytes[index + 4] = data[byteInPackets+index];
+	        	}
+	        	
+	        	DatagramPacket sendPacket = new DatagramPacket(bytes, bytes.length, host, targetPort);
 				datagramSocket.send(sendPacket);
-			}
-			System.out.println("Sender: transfer completed");
-	        // socket.close();
-			datagramSocket.close();
-	        fis.close();
+	        	
+				System.out.println("Sending Packet no " + packetNumber);
+				
+				boolean isAckReceivedCorrect = false;
+				while(!isAckReceivedCorrect ) { //check the received ack from reciever is correct or not 
+												//if not then resend the packet
+					if(CheckArk(packetNumber, isAckReceivedCorrect, AckReceived, datagramSocket, sendPacket)==-1) break;
+					
+				}
+	        }
+				
+	        	datagramSocket.close();
+	            System.out.println("File has been sent.");
+	        
 	}
-		
+
+	private static int CheckArk(int packetNumber, boolean isAckReceivedCorrect, boolean AckReceived,
+			DatagramSocket datagramSocket, DatagramPacket sendPacket) throws IOException {
+		byte[] Ack = new byte[2];
+		DatagramPacket Acks = new DatagramPacket(Ack, Ack.length);
+		datagramSocket.receive(Acks);
+		if(AckReceived=true) {
+			isAckReceivedCorrect = true;
+			System.out.println("Ack received!");
+			return -1;
+		}else {
+			datagramSocket.send(sendPacket);
+			System.out.println("Resending the packet lost (number) " + packetNumber);
+		}
+		return 0;
 	}
+}
